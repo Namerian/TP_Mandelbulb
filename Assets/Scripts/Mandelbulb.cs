7ff24440ab2627cc322a11f7cf2a91e6bfa041ce
Unknown <patrickmonville@hotmail.com>
2017-04-24 12:09:29 +0200

    quick backup

---
+++ Assets/Scripts/Mandelbulb.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mandelbulb : MonoBehaviour {
+
+    [SerializeField]
+    private ComputeShader _shader;
+
+    private RenderTexture _bufferTexture;
+    private int _kernelIndex;
+
+	// Use this for initialization
+	void Start () {
+        _kernelIndex = _shader.FindKernel("CSMain");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
